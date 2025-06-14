9f6e2a444e7994fa11a483b144330f2b3dc8c147
TSBC <57209317+tenstudiosbc@users.noreply.github.com>
2025-06-14 23:28:48 +0700

    PWA for Android and PCs

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'tsbc-ai-art-pwa-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/css/main.css',
+  '/css/forms.css',
+  '/js/bundle.js',
+  '/manifest.json',
+  '/favicon.png',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
+  );
+});
