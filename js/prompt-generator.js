c9c3c019bbd213ac072426646475f63d53fa8f80
TSBC <57209317+tenstudiosbc@users.noreply.github.com>
2025-06-15 11:48:35 +0700

    test

--- js/prompt-generator.js
+++ js/prompt-generator.js
@@ -104,10 +104,11 @@ function buildPrompt(data) {
     } else if (data.age) {
         characterDesc.push(data.age);
     } else if (data.gender) {
-        characterDesc.push(data.gender);        
+        characterDesc.push(data.gender);                
     }
     if (data.personality) characterDesc.push(`with a ${data.personality} personality`);
     
+       
     // Body type and features
     if (data.bodyType) characterDesc.push(`${data.bodyType} body`);
     
